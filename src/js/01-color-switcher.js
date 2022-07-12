f5826f8d99ffcb6a0152aa39fe084917d2f5f711
BondAndrii <CitrusOk777@gmail.com>
2022-07-12 21:05:54 +0300

    create 1st hw in 9 lesson

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,42 @@
 
 const startBtn = document.querySelector('[data-start]');
-console.log(startBtn);
+// console.log(startBtn);
 const stopBtn = document.querySelector('[data-stop]');
-console.log(stopBtn);
-// startBtn.addEventListener('click', )
-// stopBtn.addEventListener('click')
-// body.style.color = #455678;
-console.log(document.body.style.color);
-document.body.style.color = (186, 186, 186);
-
-function changeStyle() {
-  const color = getRandomHexColor();
-  console.log(color);
-  document.body.style.color = color;
-}
-
-changeStyle();
-
-
-
-
-
+// console.log(stopBtn);
+const styleBody = document.body.style;
+startBtn.addEventListener('click', changeStyle);
+stopBtn.addEventListener('click', stopChangeStyle);
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-console.log(getRandomHexColor());
+// console.log(getRandomHexColor());
+
+function changeStyle() {
+  timeoutId = setInterval(() => {
+    const color = getRandomHexColor();
+    console.log(color);
+    document.body.style.backgroundColor = color;
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+function stopChangeStyle() {
+  clearInterval(timeoutId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+// body.style.color = #455678;
+// console.log(document.body.style.color);
+// document.body.style.backgroundColor = 
+
+// function changeStyle() {
+//   const color = getRandomHexColor();
+//   console.log(color);
+//   document.body.style.backgroundColor = color;
+// }
+
+// changeStyle();
+
+
+
+
