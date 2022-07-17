71e1df0a894fe91867d7a960910592085f559ae2
BondAndrii <CitrusOk777@gmail.com>
2022-07-17 15:43:19 +0300

    correct 9,3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,8 +7,8 @@ const formRef = document.querySelector('.form');
 formRef.addEventListener('submit', event => {
     event.preventDefault();
     const firstDelay = +event.target.elements.delay.value;
-    const stepDelay = +event.target.step.delay.value;
-    const amount = + event.target.amount.dalay.value;
+    const stepDelay = +event.target.elements.step.value;
+    const amount = +event.target.elements.amount.value;
     let delay = firstDelay;
 
     for (let i = 1; i <= amount; i+=1, delay += stepDelay) {
@@ -23,7 +23,7 @@ function createPromise(position, delay) {
     return new Promise((resolve, reject) => {
         const shouldResolve = Math.random() > 0.3;
         setTimeout(() =>{
-  if (shouldResolve) {
+    if (shouldResolve) {
             resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
         } else {
             reject(`❌ Rejected promise ${position} in ${delay}ms`);
