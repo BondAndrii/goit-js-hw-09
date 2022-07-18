c3f7ac5fd2c5da10c4a04d958a3314dd254d711c
BondAndrii <CitrusOk777@gmail.com>
2022-07-18 11:13:03 +0300

    correct 9.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,7 +10,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 // console.log(getRandomHexColor());
-
+let timeoutId = null;
 function changeStyle() {
   timeoutId = setInterval(() => {
     const color = getRandomHexColor();
